889dec98526622727bd85aa450730ea1f10a005c
josemoracard <jose.cardinales@gmail.com>
2024-03-18 17:12:14 +0000

    fixed text, code in app.py, explore.ipynb, solution.ipynb

--- src/app.py
+++ src/app.py
@@ -1,15 +1,8 @@
 import os
-from sqlalchemy import create_engine
-import pandas as pd
-from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+import requests
+import time
+import sqlite3
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# load the .env file variables
-load_dotenv()
-
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
-
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
